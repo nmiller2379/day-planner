99626d7aad041da4503de54c18846db462c6b61c
Miller <nmiller2379@gmail.com>
2020-10-24 22:04:41 -0600

    added dynamically updated current day at top of the application

--- script.js
+++ script.js
@@ -39,12 +39,16 @@
 
 
 
-
+//Declare variables in global memory
 var time = $(".time-block") 
 var calendarTime = (document.querySelector(".hour"))
 var computerTime = parseInt(moment().format("HH"))
+var currentDay = (moment().format("MMMM Do YYYY"))
 // console.log(computerTime)
-console.log(calendarTime)
+// console.log(calendarTime)
+//console.log(currentDay)
+//Display the current day at the top of the app based on moment()
+$("#currentDay").text(currentDay)
 
 
 
@@ -54,7 +58,7 @@ console.log(calendarTime)
 // console.log(moment().format("HH"))
 // console.log(calendarTime.dataset.time)
 // console.log(typeof moment().format("HH"))
-console.log(calendarTime.dataset.time)
+// console.log(calendarTime.dataset.time)
 
 // if (calendarTime === moment().format("HH")) {
 //     console.log("In the present");
@@ -68,16 +72,16 @@ console.log(calendarTime.dataset.time)
 // } else {console.log("Not in the past.")
     
 // }
-
+//Declare function used to determine relationship between current time and the timeblock on the calendar
 function checkTime() {
     if (calendarTime.dataset.time < computerTime) {$(".time-check").addClass("past")}
     if (calendarTime.dataset.time > computerTime) {$(".time-check").addClass("future")}
     if (calendarTime.dataset.time === computerTime) {$(".time-check").addClass("present")}
     
 }
-
+//Change timeblocks color depending on the time of the day
 checkTime()
-// connect o momentsjs API for time/date info
+// connect to momentsjs API for time/date info
 
 // use time/date info to dynamically update page
 
